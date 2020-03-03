f5116367b5f8af1c4aadccf82bf45141944316a7
Timur Mishiev <timurmishiev@gmail.com>
2020-03-03 05:12:56 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -8,4 +8,10 @@ class MusicImporter
     Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
   end
   
+  def import
+    self.files.each do |filename|
+      Song.create_from_filename(filename)
+    end
+  end
+  
 end 
